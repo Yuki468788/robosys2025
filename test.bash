c46b0eaab56ec0f8ec358cedf784aca60712344a
Yuki468788 <s24c1006pt@s.chibakoudai.jp>
2025-10-30 12:55:49 +0900

    Add a command

---
+++ test.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+# SPDX- FileCopyrightText: 2025 Yuki Akutsu
+# SPDX-License-Identifier: BSD-3-Clause
+ng () {
+        echo ${1}行目が違うよ
+        res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
+
+exit $res
+
