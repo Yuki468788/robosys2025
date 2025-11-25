63183de3f5277d9b0f1edf279967a43d76b233af
Yuki468788 <s24c1006pt@s.chibakoudai.jp>
2025-11-25 15:15:52 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,22 +1,23 @@
 #!/bin/bash
-# SPDX- FileCopyrightText: 2025 Yuki Akutsu
+# SPDX-FileCopyrightText: 2025 Yuki Akutsu
 # SPDX-License-Identifier: BSD-3-Clause
+
 ng () {
-        echo ${1}行目が違うよ
-        res=1
+      echo ${1}行目が違うよ
+      res=1
 }
 
 res=0
 
-out=$(seq 5 | ./plus)
+out=$(echo 10 | ./thunder)
+[ "${out}" = "3405.0" ] || ng "$LINENO"
 
-[ "${out}" = 15 ] || ng "$LINENO"
+out=$(echo "10 0" | ./thunder)
+[ "${out}" = "3315.0" ] || ng "$LINENO"
 
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]  || ng  "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+out=$(echo あ | ./thunder)
+[ "${out}" = "0" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 
 exit $res
-
